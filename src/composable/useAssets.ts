9d814e8aa90dfdcee64a4c03d8f8f27d1075e796
binanStar <williamleo0906@gmail.com>
2021-07-24 00:09:19 +0300

    test with dist

--- src/composable/useAssets.ts
+++ src/composable/useAssets.ts
@@ -4,18 +4,23 @@ export default function useAssets() {
   const svgs =
     env === 'development'
       ? import.meta.globEager('/src/assets/*.svg')
-      : import.meta.globEager('/*.svg');
+      : import.meta.globEager('/dist/assets/*.svg');
 
   const pngs =
     env === 'development'
       ? import.meta.globEager('/src/assets/*.png')
       : import.meta.globEager('/*.png');
 
-  console.log(svgs);
-  console.log(pngs);
+  // for netli
+  var ce = import.meta.globEager('/dist/assets/*.svg');
+  console.log(ce);
+  var ce2 = import.meta.globEager('/assets/*.svg');
+  console.log(ce2);
+
+  const imgUrl = new URL('aboutImage.png', import.meta.url);
+
   const about =
     svgs[env === 'development' ? '/src/assets/aboutImage.svg' : 'aboutImage.svg'].default;
-  console.log(about);
 
   return {
     about,
